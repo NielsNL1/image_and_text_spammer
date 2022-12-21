e475a6f6e305d5f029c3437ad194dcb4f5e7bfb1
Niels <111445696+NielsNL1@users.noreply.github.com>
2022-12-21 20:19:41 +0100

    Create main.test.py

---
+++ main.test.py
@@ -0,0 +1,21 @@
+# imports libraries
+from PIL import Image
+import requests
+from io import BytesIO
+
+# defines URL
+url = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2F7ZX9JMY_k2E%2Fmaxresdefault.jpg" \
+      "&f=1&nofb=1&ipt=fb9279e17f046cf240011cbb1ee03ee33adf1c7ff5574e62035d006c270cbf6b&ipo=images "
+
+
+# gets the URL and defines image_spam
+def image_spam():
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
+    img.show()
+
+
+# creates the infinite loop
+while True:
+    print('test')
+    print(image_spam)
